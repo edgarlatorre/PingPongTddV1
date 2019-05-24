2ee83c81c8879433d129c4c4233e1e90c8ab172d
David Stanete <davidstanete@gmail.com>
2019-05-24 15:39:44 +0200

    Test creates movies when data is correct

--- src/videoclub/movies/tests/test_views.py
+++ src/videoclub/movies/tests/test_views.py
@@ -1,9 +1,11 @@
 import pytest
 from rest_framework import status
 
+from videoclub.movies.models import Movie
+
 
 @pytest.mark.django_db
-class TestGetMovies:
+class TestGetMovie:
 
     def test_returns_movie_when_movie_exists(self, client, movie):
         response = client.get(f'/movies/{movie.id}')
@@ -28,3 +30,30 @@ class TestListMovies:
 
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == []
+
+    @pytest.mark.usefixtures('movie')
+    def test_returns_movies_list_when_movies_exist(self, client):
+        response = client.get('/movies/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == [
+            {
+                'title': 'John Wick',
+                'price': '10.00',
+            }
+        ]
+
+
+@pytest.mark.usefixtures
+class TestCreateMovie:
+
+    def test_creates_movie_when_data_is_valid(self, client):
+        data = {
+            'title': 'Top Gun',
+            'price': '7.95',
+        }
+
+        response = client.post('/movies/', data)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Movie.objects.filter(title='Top Gun', price='7.95').exists()
