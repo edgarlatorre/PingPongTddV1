7969ba22998dff9e825b649c5462a012f8e029d3
David Stanete <davidstanete@gmail.com>
2019-05-31 11:29:24 +0200

    Admin fields for new MovieAdmin

---
+++ src/videoclub/movies/tests/test_admin.py
@@ -0,0 +1,19 @@
+import pytest
+from django.contrib.admin import AdminSite
+
+from videoclub.movies.admin import MovieAdmin
+from videoclub.movies.models import Movie
+
+
+class TestMovieAdmin:
+
+    @pytest.fixture
+    def admin_site(self):
+        return AdminSite()
+
+    @pytest.fixture
+    def movie_admin(self, admin_site):
+        return MovieAdmin(Movie, admin_site)
+
+    def test_fields_are_set(self, movie_admin):
+        assert movie_admin.fields == ('title', 'price', 'score',)
