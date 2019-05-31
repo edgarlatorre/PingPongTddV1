cb9a90edf3c6fcd18277723db34cee06a176c0b7
David Stanete <davidstanete@gmail.com>
2019-05-31 12:01:56 +0200

    Add genre field to model

--- src/videoclub/movies/tests/test_models.py
+++ src/videoclub/movies/tests/test_models.py
@@ -6,14 +6,19 @@ from ..models import Movie
 @pytest.mark.django_db
 class TestMovie:
 
-    def test_a_movie_is_created_when_passing_title_and_price(self):
+    def test_creates_a_movie_when_passing_title_and_price(self):
         Movie.objects.create(title='Titanic', price='3.95')
 
         assert Movie.objects.count() == 1
 
-    def test_a_movie_is_created_with_default_score_to_zero(self):
+    def test_creates_a_movie_with_default_score_to_zero(self):
         Movie.objects.create(title='Titanic', price='3.95')
 
-        movie = Movie.objects.first()
+        movie = Movie.objects.get(title='Titanic', price='3.95')
         assert movie.score == 0
 
+    def test_creates_a_movie_with_no_genre(self):
+        Movie.objects.create(title='Titanic', price='3.95')
+
+        movie = Movie.objects.get(title='Titanic', price='3.95')
+        assert movie.genre is None
