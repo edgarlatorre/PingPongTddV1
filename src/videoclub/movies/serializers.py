1647c9c1a6a60ef837f28a5af761012e599e2df7
David Stanete <davidstanete@gmail.com>
2019-05-31 11:44:15 +0200

    Make score read only for movie

--- src/videoclub/movies/serializers.py
+++ src/videoclub/movies/serializers.py
@@ -8,12 +8,13 @@ class MovieSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
-        fields = ('title', 'price')
+        fields = ('title', 'price', 'score', )
+        read_only_fields = ('score', )
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
 
-        if flag.is_active('show_ratings'):
-            representation['score'] = instance.score
+        if not flag.is_active('show_ratings'):
+            del representation['score']
 
         return representation
