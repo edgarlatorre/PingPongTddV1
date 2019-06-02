b04c404ba61cfff49afd7d88c467092d052e7b45
Daniel Ruiz <druizca@mercadona.es>
2019-06-02 20:35:39 +0200

    Send OMDB's api key in all requests

--- src/videoclub/movies/tests/test_clients.py
+++ src/videoclub/movies/tests/test_clients.py
@@ -4,7 +4,9 @@ from ..clients import OMDBClient
 class TestOMDBClient:
 
     def test_returns_movie_rating_when_passing_valid_title(self, responses):
-        responses.add(responses.GET, 'http://www.omdbapi.com', status=200,
+        OMDBClient.API_KEY = 'my-api-key'
+        request_url = 'http://www.omdbapi.com/?apikey=my-api-key&t=Die+Hard'
+        responses.add(responses.GET, request_url, match_querystring=True, status=200,
                       json={'Title': 'Die Hard', 'imdbRating': '8.2'})
 
         rating = OMDBClient().get_rating_for_movie(title='Die Hard')
--- src/videoclub/settings.py
+++ src/videoclub/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# OMDB
+API_KEY_FILE = os.path.join(os.path.dirname(BASE_DIR), '.api-key')
+API_KEY = open(API_KEY_FILE).read().strip()
